e0f4264a89bf92482d2974ed729045c64c64da55
Daniel Green <dan@shup.com>
2011-01-22 20:49:37 -0800

    fixing include path

--- explosivo/png.h
+++ explosivo/png.h
@@ -1,7 +1,7 @@
 #ifndef _SHUP_PNG_H
 #define _SHUP_PNG_H
 
-#include "../../pnglib/png.h"
+#include "../pnglib/png.h"
 #include "./memblock.h"
 
 int savePNG(const char *filename, int *bits, int w, int h, int wantalpha);

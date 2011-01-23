a8e5ab72e636ecfc7e20648e1f18a0b47d6e6f63
Daniel Green <dan@shup.com>
2011-01-22 23:48:49 -0800

    fixing png includes

--- explosivo/png.h
+++ explosivo/png.h
@@ -1,7 +1,10 @@
 #ifndef _SHUP_PNG_H
 #define _SHUP_PNG_H
 
-#include "../pnglib/png.h"
+#define png_infopp_NULL (png_infopp)NULL
+#define int_p_NULL (int*)NULL
+
+#include "../libpng/png.h"
 #include "./memblock.h"
 
 int savePNG(const char *filename, int *bits, int w, int h, int wantalpha);
